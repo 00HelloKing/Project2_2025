627f5f184e6e1cd6667da9ae7fe48eb22f98fae7
00HelloKing <jkf1018407510@163.com>
2025-04-21 10:33:44 +0800

    Added information with comments in SoilSensor.py

--- SoilSensor.py
+++ SoilSensor.py
@@ -1,21 +1,31 @@
+# Rasberry Pi Plant Moisture Sensor
+# It can check if there is water from soil
+# Name: Jin Kaifeng
+# Student ID: 202283890029
+# Semester Project 3  &  Grade 3
+# Date: 20/4/25
+
 import RPi.GPIO as GPIO
 import time
 
-#GPIO SETUP
+# GPIO setup
 channel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+# show information about watering
 def callback(channel):
 	if GPIO.input(channel):
 		print("Water Not Detected")
 	else:
 		print("Water Detected")
-#let us know when the pin goes HIGH or LOW
+
+# Let us know when the pin goes HIGH or LOW
 GPIO.add_event_detect(channel, GPIO.BOTH,bouncetime=300)
-#assign function to GPIO PIN,Run function on change
+
+# Assign function to GPIO PIN,Run function on change
 GPIO.add_event_callback(channel, callback)
 
-#infinite loop
+# Infinite loop
 while True:
 	time.sleep(0)
