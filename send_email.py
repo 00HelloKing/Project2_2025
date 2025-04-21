da5a1baf0a019cb56cc496c677894fc8041b8212
00HelloKing <jkf1018407510@163.com>
2025-04-21 10:28:20 +0800

    Added information with comments in send_email.py

--- send_email.py
+++ send_email.py
@@ -1,3 +1,10 @@
+# Email Notification
+# It sends emails to inform the user with SMTP
+# Name: Jin Kaifeng
+# Student ID: 202283890029
+# Semester Project 3  &  Grade 3
+# Date: 20/4/25
+
 import smtplib
 from email.message import EmailMessage
 
@@ -11,6 +18,7 @@ body = "Hello from Raspberry Pi"
 msg.set_content(body)
 msg['From'] = from_email_addr
 msg['To'] = to_email_addr
+# The subject of emails
 msg['Subject'] = 'TEST EMAIL'
 
 # the port
@@ -19,4 +27,5 @@ server.starttls()
 server.login(from_email_addr, from_email_pass)
 server.send_message(msg)
 print('Email sent')
+# quit the server
 server.quit()
